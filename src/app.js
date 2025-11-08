50fae22e33dad3eeff8a2d5ce98b732afca51b4d
Arun Sai Narla <arunsaiknr@gmail.com>
2025-11-08 17:45:03 -0500

    CRUD Operation and Postman API Testing

--- src/app.js
+++ src/app.js
@@ -4,10 +4,27 @@ const port = 8080;
 
  app.listen(port, () =>{console.log(`Server started on port ${port}`)});
 
+ // GET
  app.get("/", (req,res) => {
     res.send("Hello from the dashboard !!");
+ });
+
+ // POST
+ app.post("/data", (req,res) =>{
+    res.send("Data saved to the database successfully!")
+ });
+
+ // DELETE
+ app.delete("/file", (req,res) =>{
+    res.send("The File has been successfully delted !!")
  });
 
  app.get("/test", (req,res) =>{
     res.send("Hey there, this is the endpoint of the /test api");
  })
+
+
+ 
+
+
+
