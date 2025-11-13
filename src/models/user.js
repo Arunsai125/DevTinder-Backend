92dcf7286cd62f9551495bfba789a793d5a15585
Arun Sai Narla <arunsaiknr@gmail.com>
2025-11-12 21:50:18 -0500

    Added userSchema

---
+++ src/models/user.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+    firstName : {type: String},
+    lastName : {type:String},
+    emailId : {type:String},
+    password: {type:String},
+    age: {type: Number},
+    gender : {type:String}
+});
+
+const userModel = mongoose.model("User", userSchema);
