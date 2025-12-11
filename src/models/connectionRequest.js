f056666be83ef287fdc7c7f296e21f84c0623b2b
Arun Sai Narla <arunsaiknr@gmail.com>
2025-12-10 20:14:10 -0500

    Refactor connection request validation; improve readability of self-request check

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -23,7 +23,7 @@ connectionRequestSchema.index({fromUserId:1, toUserId:1});
 
 connectionRequestSchema.pre("save", function(next){
     const connectionRequest = this;
-    if(this.fromUserId.equals(this.toUserId)){
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
         throw new Error("Hey Attacker, You cant send yourself a connection request!!");
     }
     next();
