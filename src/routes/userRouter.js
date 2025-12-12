4a7add38c04b7d28fa2a67f60610d727ff254f60
Arun Sai Narla <arunsaiknr@gmail.com>
2025-12-11 20:46:58 -0500

    Enhance connectionRequest schema by adding missing ref for toUserId and fix response message formatting in userRouter

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -11,8 +11,7 @@ userRouter.get("/user/requests/received", userAuth, async(req, res) =>{
             toUserId : loggedInUser._id,
             status: "interested"
         }).populate("fromUserId", ["firstName" , "lastName"]);
-        res.json({message : `Hey ${loggedInUser.firstName}, Here's the data of all the connection 
-            request that are waiting for approval`, data: {connectionsRecieved}});
+        res.json({message : `Hey ${loggedInUser.firstName}, Here's the data of all the connection requests that are waiting for approval`, data: {connectionsRecieved}});
     }
     catch(err){
         res.status(400).send("Something went wrong !!" + err.message);
@@ -24,8 +23,10 @@ userRouter.get("/user/connections", userAuth, async(req,res) =>{
         const loggedInUser = req.user;
         const userConnections = await connectionRequestSchemaModel.find
         ({$or: [{toUserId : loggedInUser._id}, {fromUserId : loggedInUser._id}], status : "accepted"})
-        .populate("fromUserId", ["firstName", "lastName"]);
-        const data = userConnections.map( (row) => row.fromUserId);
+        .populate("fromUserId", ["firstName", "lastName"]).populate("toUserId", ["firstName", "lastName"]);
+        const data = userConnections.map((row) => {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()) return row.toUserId;
+            return row.fromUserId});
         res.json({message: `Hey ${loggedInUser.firstName}, Here's the data of your ${userConnections.length} connections`, data: {data}});
 
     }
