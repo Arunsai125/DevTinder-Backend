179d7234e102e20f0dc266774854b230670a3da8
Arun Sai Narla <arunsaiknr@gmail.com>
2025-12-15 18:08:44 -0500

    Add user feed endpoint to suggest potential connections based on existing user relationships

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,7 @@
 import express from  "express";
 import userAuth from  "../middleware/auth.js";
 import connectionRequestSchemaModel from "../models/connectionRequest.js";
+import userModel from "../models/user.js";
 
 const userRouter = express.Router();
 
@@ -32,6 +33,30 @@ userRouter.get("/user/connections", userAuth, async(req,res) =>{
     }
     catch(err){
         res.status(400).send("Something went wrong: " + err.message);
+    }
+});
+
+userRouter.get("/user/feed", userAuth, async (req,res) =>{
+    try{
+        const loggedInUser = req.user;
+        // Get All the User Connections that loggedInUser has sent or ignored or accepted or rejected
+        const userConnections = await connectionRequestSchemaModel.find(
+            {$or : [{fromUserId : loggedInUser._id},{toUserId : loggedInUser._id}]}
+        ).select("fromUserId toUserId").populate("fromUserId", ["firstName", "lastName"]).populate("toUserId", ["firstName", "lastName"]);
+
+        // A new Set that stores unique id's of all the user connections with the user himself
+        const toBeExcluded = new Set();
+        userConnections.forEach((connection) => {toBeExcluded.add(connection.fromUserId._id.toString()); toBeExcluded.add(connection.toUserId._id.toString());});
+
+        // Now we return all the users from the user db that aren't included in the set
+        const userFeedData = await userModel.find({
+            _id:{$nin:Array.from(toBeExcluded)}
+        }).select(["_id", "firstName", "lastName", "age"]);
+
+        res.json({message : `Hey ${loggedInUser.firstName}, Here are the list of few people you might be interested in`, data: {userFeedData} });
+    }
+    catch(err){
+        res.status(400).send("Here's where it failed: " + err.message);
     }
 });
 
