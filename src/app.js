bf8152f3a67e7b2ec8b54639ccbc8f55aedeea04
Arun Sai Narla <arunsaiknr@gmail.com>
2025-12-18 20:27:06 -0500

    Fix CORS origin URL in app.js to include the correct protocol

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ connectDB().then(
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http:localhost:5173", credentials:true
+    origin:"http://localhost:5173", credentials:true
 }));
 app.use("/", authRouter);
 app.use("/", profileRouter);
